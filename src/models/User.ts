80e01c4d8bd22c592632cb642ec33e17ff9abb95
Shourya Singh Sengar <Shouryacode2021@gmail.com>
2024-04-14 03:01:02 +0530

    feat(models): Add User model

---
+++ src/models/User.ts
@@ -0,0 +1,11 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+/*
+ * Interface (Message) defines the TypeScript type for the data.
+ * It helps maintain type safety and provides IntelliSense support during development.
+ */
+
+/*
+ * Schema (MessageSchema) defines the structure and constraints for storing that data in MongoDB. 
+ * It helps enforce data integrity and consistency within the database.
+ */
